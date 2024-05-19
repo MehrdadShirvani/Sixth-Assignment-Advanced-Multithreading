2f8a5cdbd81231b1d0f2977f5a1cc4a3c559501d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-05-19 13:15:48 +0330

    Completed Semaphore Assignment

--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -4,7 +4,9 @@ public class Resource {
 
     public static void accessResource() {
         try {
+            Controller.semaphore.acquire();
             Thread.sleep(100);
+            Controller.semaphore.release();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
